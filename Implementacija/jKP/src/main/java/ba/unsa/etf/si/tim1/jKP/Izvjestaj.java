7964c982aff7e403c0b838f9a5cb51e2fd006c87
AhmAli <ahmedalic@hotmail.com>
2014-05-12 00:53:30 +0200

    Create Izvjestaj.java

---
+++ Implementacija/jKP/src/main/java/ba/unsa/etf/si/tim1/jKP/Izvjestaj.java
@@ -0,0 +1,21 @@
+package jKP;
+import java.util.Date;
+
+public  class Izvjestaj {
+	
+	
+
+
+void mjesecniIzvjestaj(Date OdDat, Date DoDat){
+	
+}
+void godisnjiIzvjestaj (Date OdDat, Date DoDat){
+	
+}
+void izvjestajOLokacijama (Date OdDat, Date DoDat){
+	
+}
+void izvjestajOUcinkuUposlenika (Zaposlenik iouz){
+	
+}
+}
