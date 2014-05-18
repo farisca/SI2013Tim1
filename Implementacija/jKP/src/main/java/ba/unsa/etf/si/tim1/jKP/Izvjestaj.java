483a64299a52023ee2f516afff1504c993c44538
Dejan Azinovic <Dejan Azinovic@DEJAN>
2014-05-18 19:01:33 +0200

    Dodata klasa Izvjestaj.

---
+++ Implementacija/jKP/src/main/java/ba/unsa/etf/si/tim1/jKP/Izvjestaj.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.si.tim1.jKP;
+
+import java.util.Date;
+
+public class Izvjestaj {
+	public void mjesecniIzvjestaj(Date odDatuma, Date doDatuma) {}
+	public void godisnjiIzvjestaj(Date odDatuma, Date doDatuma) {}
+	public void izvjestajOLokacijama(Date odDatuma, Date doDatuma) {}
+	public void izvjestajOUcinkuZaposlenika(Zaposlenik zaposlenik) {}
+}
