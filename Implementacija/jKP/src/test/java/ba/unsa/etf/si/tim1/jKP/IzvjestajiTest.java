6e8f1cead271233aaa37ca1addcc337a8a070aa5
Faris Cakaric <fcakaric1@etf.unsa.ba>
2014-05-31 20:20:03 +0200

    Dodani testovi izvjestaj

--- Implementacija/jKP/src/test/java/ba/unsa/etf/si/tim1/jKP/IzvjestajiTest.java
+++ Implementacija/jKP/src/test/java/ba/unsa/etf/si/tim1/jKP/IzvjestajiTest.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.si.tim1.jKP;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import junit.framework.TestCase;
@@ -8,10 +9,17 @@ public class IzvjestajiTest extends TestCase {
 
 	public void testSedmicniRadniciLosDatum() {
 		try { 
-			Date date = new Date();
-			date.setYear(2100);
+			Date myDate;
+	        Calendar cal = Calendar.getInstance();
+	        cal.set(Calendar.MONTH, 9);
+	        cal.set(Calendar.DATE, 24);
+	        cal.set(Calendar.YEAR, 2100);
+	        cal.set(Calendar.HOUR,13);
+	        cal.set(Calendar.MINUTE,45);
+	        cal.set(Calendar.SECOND,52);
+	        myDate = cal.getTime();
 			Izvjestaji iz = new Izvjestaji();
-			iz.SedmicniRadnici("proba", date);
+			iz.SedmicniRadnici("proba", myDate);
 			fail("Nije bacen izuzetak."); 
 		} catch (Exception e) { 
 			 assertTrue(true); 
@@ -20,10 +28,17 @@ public class IzvjestajiTest extends TestCase {
 	
 	public void testSedmicniRadniciDobarDatum() {
 		try { 
-			Date date = new Date();
-			date.setYear(2013);
+			Date myDate;
+	        Calendar cal = Calendar.getInstance();
+	        cal.set(Calendar.MONTH, 9);
+	        cal.set(Calendar.DATE, 24);
+	        cal.set(Calendar.YEAR, 2013);
+	        cal.set(Calendar.HOUR,13);
+	        cal.set(Calendar.MINUTE,45);
+	        cal.set(Calendar.SECOND,52);
+	        myDate = cal.getTime();
 			Izvjestaji iz = new Izvjestaji();
-			iz.SedmicniRadnici("proba", date);
+			iz.SedmicniRadnici("proba", myDate);
 			assertTrue(true); 
 		} catch (Exception e) { 
 			fail(e.getMessage()); 
@@ -32,10 +47,17 @@ public class IzvjestajiTest extends TestCase {
 	
 	public void testGodisnjiSumarniLosDatum() {
 		try { 
-			Date date = new Date();
-			date.setYear(2100);
+			Date myDate;
+	        Calendar cal = Calendar.getInstance();
+	        cal.set(Calendar.MONTH, 9);
+	        cal.set(Calendar.DATE, 24);
+	        cal.set(Calendar.YEAR, 2100);
+	        cal.set(Calendar.HOUR,13);
+	        cal.set(Calendar.MINUTE,45);
+	        cal.set(Calendar.SECOND,52);
+	        myDate = cal.getTime();
 			Izvjestaji iz = new Izvjestaji();
-			iz.Godisnji("proba", date);
+			iz.Godisnji("proba", myDate);
 			fail("Nije bacen izuzetak."); 
 		} catch (Exception e) { 
 			 assertTrue(true); 
@@ -44,14 +66,98 @@ public class IzvjestajiTest extends TestCase {
 	
 	public void testGodisnjiSumarniDobarDatum() {
 		try { 
-			Date date = new Date();
-			date.setYear(2013);
+			Date myDate;
+	        Calendar cal = Calendar.getInstance();
+	        cal.set(Calendar.MONTH, 9);
+	        cal.set(Calendar.DATE, 24);
+	        cal.set(Calendar.YEAR, 2013);
+	        cal.set(Calendar.HOUR,13);
+	        cal.set(Calendar.MINUTE,45);
+	        cal.set(Calendar.SECOND,52);
+	        myDate = cal.getTime();
+	        
 			Izvjestaji iz = new Izvjestaji();
-			iz.Godisnji("proba", date);
+			iz.Godisnji("proba", myDate);
 			assertTrue(true); 
 		} catch (Exception e) { 
 			fail(e.getMessage()); 
 		} 
 	}
 
+	public void testMjesecniViseLokacijaLosDatum() {
+		try { 
+			Date myDate;
+	        Calendar cal = Calendar.getInstance();
+	        cal.set(Calendar.MONTH, 9);
+	        cal.set(Calendar.DATE, 24);
+	        cal.set(Calendar.YEAR, 2100);
+	        cal.set(Calendar.HOUR,13);
+	        cal.set(Calendar.MINUTE,45);
+	        cal.set(Calendar.SECOND,52);
+	        myDate = cal.getTime();
+			Izvjestaji iz = new Izvjestaji();
+			iz.SedmicniRadnici("proba", myDate);
+			fail("Nije bacen izuzetak."); 
+		} catch (Exception e) { 
+			 assertTrue(true); 
+		} 
+	}
+	
+	public void testMjesecniViseLokacijaDobarDatum() {
+		try { 
+			Date myDate;
+	        Calendar cal = Calendar.getInstance();
+	        cal.set(Calendar.MONTH, 9);
+	        cal.set(Calendar.DATE, 24);
+	        cal.set(Calendar.YEAR, 2013);
+	        cal.set(Calendar.HOUR,13);
+	        cal.set(Calendar.MINUTE,45);
+	        cal.set(Calendar.SECOND,52);
+	        myDate = cal.getTime();
+			Izvjestaji iz = new Izvjestaji();
+			iz.SedmicniRadnici("proba", myDate);
+			assertTrue(true); 
+		} catch (Exception e) { 
+			fail(e.getMessage()); 
+		} 
+	}
+	
+	public void testMjesecniStorniraniLosDatum() {
+		try { 
+			Date myDate;
+	        Calendar cal = Calendar.getInstance();
+	        cal.set(Calendar.MONTH, 9);
+	        cal.set(Calendar.DATE, 24);
+	        cal.set(Calendar.YEAR, 2100);
+	        cal.set(Calendar.HOUR,13);
+	        cal.set(Calendar.MINUTE,45);
+	        cal.set(Calendar.SECOND,52);
+	        myDate = cal.getTime();
+			Izvjestaji iz = new Izvjestaji();
+			iz.Godisnji("proba", myDate);
+			fail("Nije bacen izuzetak."); 
+		} catch (Exception e) { 
+			 assertTrue(true); 
+		} 
+	}
+	
+	public void testMjesecniStorniraniDobarDatum() {
+		try { 
+			Date myDate;
+	        Calendar cal = Calendar.getInstance();
+	        cal.set(Calendar.MONTH, 9);
+	        cal.set(Calendar.DATE, 24);
+	        cal.set(Calendar.YEAR, 2013);
+	        cal.set(Calendar.HOUR,13);
+	        cal.set(Calendar.MINUTE,45);
+	        cal.set(Calendar.SECOND,52);
+	        myDate = cal.getTime();
+	        
+			Izvjestaji iz = new Izvjestaji();
+			iz.Godisnji("proba", myDate);
+			assertTrue(true); 
+		} catch (Exception e) { 
+			fail(e.getMessage()); 
+		} 
+	}
 }
